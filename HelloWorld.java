fbfd12aee0c37c6b5625d46d977479571a234c74
Yash Chaudhari <yashchaudhari134@gmail.com>
2025-04-05 18:35:52 +0530

    1st commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public void printHello() {
+        System.out.println("Hello World!!");
+    }
+    public static void main(String[] args) {
+        HelloWorld h1 = new HelloWorld();
+        h1.printHello();
+    }
+}
+
+
